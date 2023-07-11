1abce372503585d96779f56ed3c0cc6f8db8a523
thiagosemann <42595307+thiagosemann@users.noreply.github.com>
2023-07-11 17:10:58 -0300

    Teste2

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
+require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
 const app = require('./app');
-require('dotenv').config();
 const WebSocket = require('ws');
 
 const PORT = process.env.PORT || 80;
@@ -8,8 +8,6 @@ const HOST = '0.0.0.0';
 const server = app.listen(PORT, HOST, () => {
   console.log('Server running on', HOST + ':' + PORT);
 });
-
-
 
 const { createWebSocketServer } = require('./websocket');
 createWebSocketServer(server);
