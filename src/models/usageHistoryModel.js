d3c1bfd5aa28557d825511968ba0197cb76e8773
Thiago <42595307+thiagosemann@users.noreply.github.com>
2023-12-16 20:37:37 -0300

    Retirado a verificação

--- src/models/usageHistoryModel.js
+++ src/models/usageHistoryModel.js
@@ -89,13 +89,6 @@ const getAllUsageHistoryByMachine = async (machineId) => {
 
 const updateUsageHistory = async (usageHistory) => {
   try {
-    // Verificar se a data está no formato correto (ISO 8601)
-    if (!isValidISO8601(end_time)) {
-      console.warn('A data não está em um formato válido. Utilizando a data atual do servidor.');
-      // Substituir end_time pela data e hora atual do servidor em formato ISO 8601
-      const currentDate = new Date().toISOString().slice(0, 19).replace("T", " ");
-      usageHistory.end_time = currentDate;
-    }
 
     const { id, end_time, total_cost } = usageHistory;
     // Atualizar a tabela UsageHistory
