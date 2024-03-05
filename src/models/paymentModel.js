2e207e5225156cd65a279df6004c51c8d12d918d
Thiago <42595307+thiagosemann@users.noreply.github.com>
2024-03-05 09:39:15 -0300

    Arrumado nome da tabela

--- src/models/paymentModel.js
+++ src/models/paymentModel.js
@@ -1,8 +1,8 @@
 const connection = require('./connection');
 
-const criarPagamento = async (preferencia) => {
-  const { user_id, valor_total, tipo_pagamento, email_comprador} = preferencia;
-  const query = 'INSERT INTO preferencia_mercado_pago (user_id, valor_total, tipo_pagamento, email_comprador) VALUES (?, ?, ?, ?)';
+const criarPagamento = async (pagamento) => {
+  const { user_id, valor_total, tipo_pagamento, email_comprador} = pagamento;
+  const query = 'INSERT INTO pagamento_mercado_pago (user_id, valor_total, tipo_pagamento, email_comprador) VALUES (?, ?, ?, ?)';
   const [result] = await connection.execute(query, [user_id, valor_total, tipo_pagamento, email_comprador]);
   return { insertId: result.insertId };
 };
